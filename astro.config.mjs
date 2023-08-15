368c5a727d0993129d1d06ff58798d1e043cfa7a
nestorbfgo <nestor.martinez@bfgo.mx>
2023-08-15 07:56:51 -0600

    add astro addapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercel(),
 });
